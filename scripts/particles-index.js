97196a60b8fdc25327fcbb4a60bbbc665e83ccd2
Ryan <RyanLow@users.noreply.github.com>
2021-01-01 13:51:39 -0500

    more small fixes

--- scripts/particles-index.js
+++ scripts/particles-index.js
@@ -1,49 +1,50 @@
-let globalColor = '#d0d0d0'
+"use strict";
 
+var globalColor = '#d0d0d0';
 tsParticles.load('window', {
-    particles: {
-        shape: {
-            type: 'circle',
-        },
-        color: {
-            value: globalColor
-        },
-        number: {
-            value: 20,
-            density: {
-                enable: true,
-                area: 1000,
-                factor: 1000
-            }
-        },
-        move: {
-            enable: true,
-            speed: 3
-        },
-        links: {
-            enable: true,
-            color: {
-                value: globalColor
-            }
-        }
+  particles: {
+    shape: {
+      type: 'circle'
     },
-    interactivity: {
-        detectsOn: "window",
-        events: {
-            onHover: {
-                enable: true,
-                mode: 'repulse'
-            }
-        },
-        modes: {
-            repulse: {
-                distance: 160,
-                speed: 0.1
-            }
-        }
+    color: {
+      value: globalColor
     },
-    backgroundMode: {
-        enable: true
+    number: {
+      value: 20,
+      density: {
+        enable: true,
+        area: 1000,
+        factor: 1000
+      }
     },
-    fpsLimit: 60
+    move: {
+      enable: true,
+      speed: 3
+    },
+    links: {
+      enable: true,
+      color: {
+        value: globalColor
+      }
+    }
+  },
+  interactivity: {
+    detectsOn: "window",
+    events: {
+      onHover: {
+        enable: true,
+        mode: 'repulse'
+      }
+    },
+    modes: {
+      repulse: {
+        distance: 160,
+        speed: 0.1
+      }
+    }
+  },
+  backgroundMode: {
+    enable: true
+  },
+  fpsLimit: 60
 });
--- scripts/typeit-index.js
+++ scripts/typeit-index.js
@@ -1,34 +1,18 @@
+"use strict";
+
 // I am...
-let descriptors = [
-    "a programmer",
-    "a mathematician",
-    "a student",
-    "a frisbee player",
-    "a food lover",
-    "an art lover",
-    "an engineer",
-    "a problem-solver",
-    "a thinker",
-    "a writer",
-    "a trouble-maker",
-    "in college",
-    "a dog lover",
-    "attentive",
-    "poor",
-    "a derp",
-    "kinda cute"
-];
-
+var descriptors = ["a programmer", "a mathematician", "a student", "a frisbee player", "a food lover", "an art lover", "an engineer", "a problem-solver", "a thinker", "a writer", "a trouble-maker", "in college", "a dog lover", "attentive", "poor", "a derp", "kinda cute"];
 document.getElementById('descriptor').innerHTML = '';
-
 new TypeIt('#descriptor', {
-    strings: descriptors.map(desc => desc + '.'),
-    speed: 100,
-    deleteSpeed: 50,
-    nextStringDelay: [2500, 1500],
-    startDelay: 1500,
-    breakLines: false,
-    lifeLike: true,
-    loop: true,
-    loopDelay: [1500, 2500]
+  strings: descriptors.map(function (desc) {
+    return desc + '.';
+  }),
+  speed: 100,
+  deleteSpeed: 50,
+  nextStringDelay: [2500, 1500],
+  startDelay: 1500,
+  breakLines: false,
+  lifeLike: true,
+  loop: true,
+  loopDelay: [1500, 2500]
 }).go();
